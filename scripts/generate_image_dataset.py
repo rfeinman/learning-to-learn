853f7357fe8d2d3762f1f4db75947ad859b93395
Reuben Feinman <rfeinman16@gmail.com>
2017-09-24 15:42:33 -0400

    initial commit of image data generation utilties and scripts

---
+++ scripts/generate_image_dataset.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+import argparse
+import warnings
+from toy_neuralnet.images import generate_dataset_parameters, generate_image
+from toy_neuralnet.util import synthesize_data
+
+def main():
+    # Generate the set of shapes, colors and textures that we will draw from
+    shape_set, color_set, texture_set = \
+        generate_dataset_parameters(args.nb_categories)
+    # Create folder where images will be stored; if exists, remove
+    if os.path.isdir(args.save_folder):
+        warnings.warn('Removing existing folder at %s' % args.save_folder)
+        shutil.rmtree(args.save_folder)
+    os.mkdir(args.save_folder)
+    # Synthesize the dataset
+    df, labels = synthesize_data(args.nb_categories, args.nb_exemplars)
+    shapes = [shape_set[i] for i in df['shape']]
+    colors = [color_set[i] for i in df['color']]
+    textures = [texture_set[i] for i in df['texture']]
+    for shape, color, texture, i in \
+            zip(shapes, colors, textures, range(len(shapes))):
+        save_file = os.path.join(args.save_folder, 'img%0.4i.png' % i)
+        generate_image(shape, color, texture, save_file)
+    # Save the dataset parameters so we know what we're working with
+    df.to_csv(os.path.join(args.save_folder, 'data.csv'))
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--nb_categories',
+                        help='The number of categories in the dataset.',
+                        required=False, type=int)
+    parser.add_argument('-e', '--nb_exemplars',
+                        help='The number of exemplars of each category in'
+                             'the dataset.',
+                        required=False, type=int)
+    parser.add_argument('-s', '--save_folder',
+                        help='The folder where ...',
+                        required=False, type=str)
+    parser.set_defaults(nb_categories=10)
+    parser.set_defaults(nb_exemplars=2)
+    parser.set_defaults(save_folder='../data/image_dataset')
+    args = parser.parse_args()
+    main()
---
+++ toy_neuralnet/images.py
@@ -0,0 +1,122 @@
+from __future__ import division, print_function
+import numpy as np
+import functools
+from random import randint
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+
+
+def rearrange_points(points):
+    """
+    A function to sort a list of points in clockwise ordering. This
+    will help to ensure that our polygon shapes are whole.
+    :param points:
+    :return:
+    """
+    center_x = np.mean([elt[0] for elt in points])
+    center_y = np.mean([elt[1] for elt in points])
+
+    # function to compare two points
+    def less(a, b):
+        # preliminary checks
+        if a[0] >= center_x and b[0] < center_x:
+            return 1
+        if a[0] < center_x and b[0] >= center_x:
+            return -1
+        if a[0] == center_x and b[0] == center_x:
+            if a[1] >= center_y or b[1] >= center_y:
+                if a[1] > b[1]:
+                    return 1
+                else:
+                    return -1
+            else:
+                if b[1] > a[1]:
+                    return 1
+                else:
+                    return -1
+
+        # compute the cross product of vectors (center -> a) x (center -> b)
+        det = (a[0] - center_x) * (b[1] - center_y) - \
+              (b[0] - center_x) * (a[1] - center_y)
+        if det < 0:
+            return 1
+        elif det > 0:
+            return -1
+
+        # Points a and b are on the same line from the center.
+        # Check which point is closer to the center.
+        d1 = (a[0] - center_x) * (a[0] - center_x) + \
+             (a[1] - center_y) * (a[1] - center_y)
+        d2 = (b[0] - center_x) * (b[0] - center_x) + \
+             (b[1] - center_y) * (b[1] - center_y)
+        if d1 > d2:
+            return 1
+        else:
+            return -1
+
+    return sorted(points, key=functools.cmp_to_key(less))
+
+def generate_random_shape(x_min, x_max, y_min, y_max, x_offset, y_offset):
+    """
+
+    :param x_min:
+    :param x_max:
+    :param y_min:
+    :param y_max:
+    :param x_offset:
+    :param y_offset:
+    :return:
+    """
+    x = np.random.uniform(x_min, x_max - x_offset)
+    y = np.random.uniform(y_min, y_max - y_offset)
+    points = [(np.random.uniform(x, x + x_offset),
+               np.random.uniform(y, y + y_offset)) for _ in
+              range(randint(3, 10))]
+    points = rearrange_points(points)
+    return points
+
+def generate_dataset_parameters(nb_shapes):
+    """
+
+    :param nb_shapes:
+    :return:
+    """
+    # Generate shapes, which are sets of points for which polygons will
+    # be generated
+    shapes = [generate_random_shape(-2, 2, -2, 2, 4, 4) for _ in
+              range(nb_shapes)]
+    # Generate colors, which are 3-D vectors of values between 0-1 (RGB values)
+    colors = np.random.uniform(0, 1, size=(nb_shapes, 3))
+    # Generate textures
+    texture_set = ['-', '+', 'x', '/', '*', 'o', 'O', '.']
+    nb_variations = int(np.ceil(nb_shapes / len(texture_set)))
+    textures = []
+    for texture in texture_set:
+        textures.extend([i * texture for i in range(1, nb_variations + 1)])
+    textures = np.random.choice(textures, nb_shapes, replace=False)
+
+    return shapes, colors, textures
+
+def generate_image(shape, color, texture, save_file):
+    """
+
+    :param shape:
+    :param color:
+    :param texture:
+    :param save_file:
+    :return:
+    """
+    # Plot figure
+    fig = plt.figure(figsize=(4, 4))
+    ax = fig.add_subplot(111)
+    # Lay the baseline color
+    polygon = Polygon(shape, closed=True, fill=True, color=color)
+    obj = ax.add_patch(polygon)
+    # Lay the texture hatch
+    polygon_h = Polygon(shape, closed=True, fill=False, color=(0, 0, 0))
+    obj_h = ax.add_patch(polygon_h)
+    obj_h.set_hatch(texture)
+    plt.ylim([-2,2])
+    plt.xlim([-2,2])
+    plt.axis('off')
+    fig.savefig(save_file)
