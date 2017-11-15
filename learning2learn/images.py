3974f3e8fdcc3bbd43ed3253981d6bf2306b49a6
Reuben Feinman <rfeinman16@gmail.com>
2017-11-15 18:53:36 -0500

    large updates to image data generation code

--- learning2learn/images.py
+++ learning2learn/images.py
@@ -1,11 +1,84 @@
-from __future__ import division
+from __future__ import division, print_function
 import numpy as np
 import functools
 from random import randint
-from PIL import Image
+from keras.preprocessing import image
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
+import matplotlib.path as mplpath
+from matplotlib.patches import (Arc, Arrow, Circle, CirclePolygon,
+                                Ellipse, Rectangle, Wedge, Polygon)
 
+class Texture:
+    """
+    TODO
+    """
+    def __init__(self, patch_type, gradient, step, params):
+        """
+
+        :param patch_type:
+        :param gradient:
+        :param step:
+        :param params:
+        """
+        assert patch_type in [
+            'ellipse', 'arc', 'arrow', 'circle',
+            'rectangle', 'wedge', 'pentagon',
+            '/', '//', '-', '--', '+', '++'
+        ], 'invalid patch_type parameter'
+        self.patch_type = patch_type
+        self.gradient = gradient
+        self.step = step
+        self.params = params
+
+    def get_patch(self, xy):
+        if self.patch_type in ['/', '//', '-', '--', '+', '++']:
+            raise Exception('No patch to return for this patch type.')
+        elif self.patch_type == 'circle':
+            return Circle(
+                xy, self.params['radius'],
+                color='black'
+            )
+        elif self.patch_type == 'rectangle':
+            return Rectangle(
+                xy, self.params['width'],
+                self.params['height'],
+                self.params['angle'],
+                color='black'
+            )
+        elif self.patch_type == 'ellipse':
+            return Ellipse(
+                xy, self.params['width'],
+                self.params['height'],
+                self.params['angle'],
+                color='black'
+            )
+        elif self.patch_type == 'arc':
+            return Arc(
+                xy, self.params['width'],
+                self.params['height'],
+                self.params['angle'],
+                color='black'
+            )
+        elif self.patch_type == 'pentagon':
+            return CirclePolygon(
+                xy, self.params['radius'],
+                5,
+                color='black'
+            )
+        elif self.patch_type == 'wedge':
+            return Wedge(
+                xy, self.params['radius'], 0,
+                self.params['theta2'],
+                color='black'
+            )
+        elif self.patch_type == 'arrow':
+            x, y = xy
+            return Arrow(
+                x, y, self.params['dx'],
+                self.params['dy'],
+                self.params['width'],
+                color='black'
+            )
 
 def rearrange_points(points):
     """
@@ -57,7 +130,8 @@ def rearrange_points(points):
 
     return sorted(points, key=functools.cmp_to_key(less))
 
-def generate_random_shape(x_min, x_max, y_min, y_max, x_offset, y_offset):
+
+def generate_random_shape(x_min, x_max, y_min, y_max, edge_distance):
     """
 
     :param x_min:
@@ -68,15 +142,199 @@ def generate_random_shape(x_min, x_max, y_min, y_max, x_offset, y_offset):
     :param y_offset:
     :return:
     """
-    x = np.random.uniform(x_min, x_max - x_offset)
-    y = np.random.uniform(y_min, y_max - y_offset)
-    points = [(np.random.uniform(x, x + x_offset),
-               np.random.uniform(y, y + y_offset)) for _ in
-              range(randint(3, 10))]
+    # Sample a number of points for the polygon
+    nb_points = randint(3, 10)
+    # 4 'types' of points; determines the edge that the point will be near
+    point_types = ['left', 'right', 'top', 'bottom']
+    # Cycle through drawing points of different types
+    points = []
+    for i in range(nb_points):
+        if point_types[i % 4] in ['left', 'right']:
+            x = np.random.uniform(0, edge_distance)
+            y = np.clip(
+                np.random.normal(loc=(y_max - y_min) / 2,
+                                 scale=(y_max - y_min) / 8),
+                y_min,
+                y_max
+            )
+            if point_types[i % 4] == 'right':
+                x = x_max - x
+        elif point_types[i % 4] in ['top', 'bottom']:
+            x = np.clip(
+                np.random.normal(loc=(x_max - x_min) / 2,
+                                 scale=(x_max - x_min) / 8),
+                x_min,
+                x_max
+            )
+            y = np.random.uniform(0, edge_distance)
+            if point_types[i % 4] == 'bottom':
+                y = y_max - y
+        points.append((x, y))
+    # Rearrange the points so that they are in the correct order
     points = rearrange_points(points)
+    #     # Now center the points by computing the mean distance
+    #     # from the center and then subtracting this mean
+    #     x_mean = np.mean([p[0] - (x_max-x_min)/2 for p in points])
+    #     y_mean = np.mean([p[1] - (y_max-y_min)/2 for p in points])
+    #     points = [(p[0]-x_mean, p[1]-y_mean) for p in points]
+
     return points
 
-def generate_dataset_parameters(nb_shapes):
+
+def generate_colors(nb_colors):
+    """
+    Function to generate a set of nb_colors colors. They
+    are generated such that there is sufficient distance
+    between each color vector. This is better than random
+    color sampling.
+    :param nb_colors: (int) the number of colors to generate
+    :return: (Numpy array) the (nb_colors, 3) color matrix
+    """
+    nb_bins = np.round(np.power(nb_colors, 1 / 3)) + 1
+    vals = np.linspace(0, 0.95, int(nb_bins))
+    colors = []
+    for r in vals:
+        for g in vals:
+            for b in vals:
+                colors.append([r, g, b])
+
+    colors = sorted(colors, key=lambda x: sum(x))
+    colors = colors[-nb_colors:]
+    return np.asarray(colors)
+
+def get_base_image(height, width, color, shape, gradient=None):
+    """
+
+    :param height:
+    :param width:
+    :param color:
+    :param shape:
+    :param gradient:
+    :return:
+    """
+    assert gradient in ['left', 'right', 'up', 'down', None]
+    # initialize image and mask
+    img = np.zeros(shape=(height, width, 3))
+    mask = np.ones(shape=(height, width, 3))
+    # find box window of the shape
+    x_min = int(min([s[0] for s in shape]))
+    x_max = int(max([s[0] for s in shape]))
+    y_min = int(min([s[1] for s in shape]))
+    y_max = int(max([s[1] for s in shape]))
+    dx = x_max - x_min
+    dy = y_max - y_min
+    # Step through and determine mask at each pixel
+    # in our shape window
+    for i in range(dy):
+        for j in range(dx):
+            if gradient == 'right':
+                mask[y_min + i, x_min + j] = j / dx
+            elif gradient == 'left':
+                mask[y_min + i, x_min + j] = 1 - j / dx
+            elif gradient == 'up':
+                mask[y_min + i, x_min + j] = 1 - i / dy
+            elif gradient == 'down':
+                mask[y_min + i, x_min + j] = i / dy
+
+    # Now step through and apply the mask
+    for i in range(height):
+        for j in range(width):
+            img[i, j] = mask[i, j] * color
+
+    return img
+
+def generate_texture(patch_type=None, image_size=500):
+    """
+
+    :param patch_type:
+    :param image_size:
+    :return:
+    """
+    if patch_type is None:
+        # randomly sample a patch type
+        patch_types = [
+            'ellipse', 'arc', 'arrow', 'circle',
+            'rectangle', 'wedge', 'pentagon',
+            '/', '//', '-', '--', '+', '++'
+        ]
+        patch_type = np.random.choice(patch_types)
+    # Patch size will be uniformly sampled. Let's define
+    # reasonable boundaries here
+    patch_min_size = int(0.03*image_size)
+    patch_max_size = int(0.09*image_size)
+    # Now, build the texture object according to the specified type
+    if patch_type == 'circle':
+        params = {
+            'radius': randint(patch_min_size, patch_max_size)
+        }
+    elif patch_type in ['rectangle', 'ellipse', 'arc']:
+        params = {
+            'height': randint(patch_min_size, patch_max_size),
+            'width': randint(2*patch_min_size, 2*patch_max_size),
+            'angle': randint(0, 180)
+        }
+    elif patch_type == 'pentagon':
+        params = {
+            'radius': randint(patch_min_size, patch_max_size)
+        }
+    elif patch_type == 'wedge':
+        params = {
+            'radius': randint(patch_min_size, patch_max_size),
+            'theta2': randint(45, 270)
+        }
+    elif patch_type == 'arrow':
+        params = {
+            'dx': randint(-patch_max_size, patch_max_size),
+            'dy': randint(-patch_max_size, patch_max_size),
+            'width': randint(3*patch_min_size, 3*patch_max_size)
+        }
+    elif patch_type in ['/', '//', '-', '--', '+', '++']:
+        params = None
+    else:
+        raise Exception('Invalid patch_type parameter.')
+
+    if patch_type in ['ellipse', 'arc', 'arrow', 'circle','rectangle',
+                      'wedge', 'pentagon']:
+        # As another parameter, step size (space between
+        # patches) will be uniformly sampled. Define
+        # boundaries here
+        step_min_size = int(0.15*image_size)
+        step_max_size = int(0.30*image_size)
+        # Now sample the step size. Later, we will step
+        # through the image placing the patches at different
+        # locations, 'step' pixels apart from one another
+        step = randint(step_min_size, step_max_size)
+    else:
+        step = None
+
+    # As a final parameter, we will sample a gradient from
+    gradient_options = [None, 'left', 'right', 'up', 'down']
+    gradient = np.random.choice(gradient_options)
+
+    # Now create the texture object instance and return
+    return Texture(patch_type, gradient, step, params)
+
+def add_texture(ax, texture, image_size=500):
+    """
+
+    :param ax:
+    :param texture:
+    :param image_size:
+    :return:
+    """
+    if texture.patch_type in ['/', '//', '-', '--', '+', '++']:
+        square = Polygon([(0, 0), (image_size, 0), (image_size, image_size),
+                          (0, image_size)],
+                         closed=True, fill=False, color=(0, 0, 0))
+        square.set_hatch(texture.patch_type)
+        ax.add_patch(square)
+    else:
+        for i in range(10, 2*image_size, texture.step):
+            for j in range(10, 2*image_size, texture.step):
+                patch = texture.get_patch((i,j))
+                ax.add_patch(patch)
+
+def generate_dataset_parameters(nb_categories, image_size=500):
     """
 
     :param nb_shapes:
@@ -84,17 +342,25 @@ def generate_dataset_parameters(nb_shapes):
     """
     # Generate shapes, which are sets of points for which polygons will
     # be generated
-    shapes = [generate_random_shape(-2, 2, -2, 2, 4, 4) for _ in
-              range(nb_shapes)]
+    shapes = [generate_random_shape(0, 500, 0, 500, 100) for _ in
+              range(nb_categories)]
     # Generate colors, which are 3-D vectors of values between 0-1 (RGB values)
-    colors = np.random.uniform(0, 1, size=(nb_shapes, 3))
+    colors = generate_colors(nb_categories)
     # Generate textures
-    texture_set = ['-', '+', 'x', '/', '*', 'o', 'O', '.']
-    nb_variations = int(np.ceil(nb_shapes / len(texture_set)))
+    patch_types = [
+            'ellipse', 'arc', 'arrow', 'circle',
+            'rectangle', 'wedge', 'pentagon'
+        ]
+    nb_variations = int(np.ceil(nb_categories / len(patch_types)))
     textures = []
-    for texture in texture_set:
-        textures.extend([i * texture for i in range(1, nb_variations + 1)])
-    textures = np.random.choice(textures, nb_shapes, replace=False)
+    for patch_type in patch_types:
+        t_list = [generate_texture(patch_type, image_size)
+                  for _ in range(nb_variations)]
+        textures.extend(t_list)
+    hatch_types = ['/', '//', '-', '--', '+', '++']
+    for hatch_type in hatch_types:
+        textures.append(generate_texture(hatch_type, image_size))
+    textures = np.random.choice(textures, nb_categories, replace=False)
 
     return shapes, colors, textures
 
@@ -107,37 +373,32 @@ def generate_image(shape, color, texture, save_file):
     :param save_file:
     :return:
     """
-    # Plot figure
-    fig = plt.figure(figsize=(4, 4))
-    ax = fig.add_subplot(111)
-    # Lay the baseline color
-    polygon = Polygon(shape, closed=True, fill=True, color=color)
-    obj = ax.add_patch(polygon)
-    # Lay the texture hatch
-    polygon_h = Polygon(shape, closed=True, fill=False, color=(0, 0, 0))
-    obj_h = ax.add_patch(polygon_h)
-    obj_h.set_hatch(texture)
-    plt.ylim([-2,2])
-    plt.xlim([-2,2])
-    plt.axis('off')
-    fig.savefig(save_file)
-
-def resize(img_array, width, height, color=True):
-    """
-    TODO
-    :param img_array:
-    :param width:
-    :param height:
-    :param color:
-    :return:
-    """
-    if color:
-        nb_channels = 3
-    else:
-        nb_channels = 1
-    # convert to PIL image
-    img = Image.fromarray(img_array)
-    # resize using PIL
-    img = img.resize((width, height), PIL.Image.ANTIALIAS)
-    # convert back to numpy array and return
-    return 1 - np.array(img.getdata()).reshape(img.size[0], img.size[1], nb_channels)
+    # Generate the base image and save it to a file
+    img = get_base_image(500, 500, color, shape, gradient=texture.gradient)
+    fig = plt.figure(frameon=False)
+    fig.set_size_inches(5, 5)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    ax.imshow(img, interpolation='bicubic')
+    add_texture(ax, texture, 500)
+    plt.savefig(save_file)
+    plt.close()
+    # Load the base image from file, crop it using mplpath,
+    # and save back to the file
+    img = image.load_img(save_file, target_size=(500, 500))
+    img = image.img_to_array(img)
+    img /= 255.
+    p = mplpath.Path(shape)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if not p.contains_point((i, j)):
+                img[j,i,:] = np.array([1.,1.,1.])
+    fig = plt.figure(frameon=False)
+    fig.set_size_inches(5, 5)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    ax.imshow(img)
+    plt.savefig(save_file, bbox_inches='tight')
+    plt.close()
