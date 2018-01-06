2a72666e26255c71939d47aec2c4c62281e4f137
Reuben Feinman <rfeinman16@gmail.com>
2018-01-06 00:12:45 -0500

    update train secondOrder script

--- scripts/cnn_train_secondOrder.py
+++ scripts/cnn_train_secondOrder.py
@@ -2,6 +2,7 @@ from __future__ import division, print_function
 import os
 import argparse
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 import keras.backend as K
 from keras.callbacks import EarlyStopping
@@ -10,76 +11,60 @@ import matplotlib as mpl
 mpl.use('Agg')
 
 from learning2learn.models import simple_cnn
-from learning2learn.images import generate_dataset_parameters, generate_image
-from learning2learn.util import (synthesize_data, synthesize_new_data,
-                                 evaluate_secondOrder, load_image_dataset)
+from learning2learn.util import evaluate_secondOrder, load_image_dataset
 
-def create_dataset(nb_categories, nb_exemplars, data_folder):
-    # Generate the set of shapes, colors and textures that we will draw from
-    shape_set, color_set, texture_set = \
-        generate_dataset_parameters(nb_categories)
-    # Create folder where images will be stored; if exists, remove
-    if os.path.isdir(data_folder):
-        print('A dataset of the specified parameters already exists. Using '
-              'the existing one...')
-    else:
-        print('Building the training dataset...')
-        os.mkdir(data_folder)
-        # Synthesize the dataset.
-        df, _ = synthesize_data(nb_categories, nb_exemplars+1)
-        shapes = [shape_set[i] for i in df['shape']]
-        colors = [color_set[i] for i in df['color']]
-        textures = [texture_set[i] for i in df['texture']]
-        for shape, color, texture, i in \
-                zip(shapes, colors, textures, range(len(shapes))):
-            save_file = os.path.join(data_folder, 'img%0.4i.png' % i)
-            generate_image(shape, color, texture, save_file)
-        # Save the dataset parameters so we know what we're working with
-        df.to_csv(os.path.join(data_folder, 'data.csv'))
-    f = os.path.join(data_folder, 'test0000/')
-    if os.path.isdir(f):
-        print('The test set already exists. Using existing one...')
-    else:
-        print('The test sets do not exist. Building the test sets...')
-        # Now Synthesize the test datasets
-        df_test, _ = synthesize_new_data(nb_categories)
-        shape_set_test, color_set_test, texture_set_test = \
-            generate_dataset_parameters(3*nb_categories)
-        shapes_test = [shape_set_test[i-nb_categories]
-                       for i in df_test['shape']]
-        colors_test = [color_set_test[i-nb_categories]
-                       for i in df_test['color']]
-        textures_test = [texture_set_test[i-nb_categories]
-                         for i in df_test['texture']]
-        for i in range(int(len(df_test)/4)):
-            test_folder = os.path.join(data_folder, 'test%0.4i' % i)
-            os.mkdir(test_folder)
-            img_file = os.path.join(test_folder, '0-base.png')
-            generate_image(shapes_test[i*4], colors_test[i*4],
-                           textures_test[i*4], img_file)
-            img_file = os.path.join(test_folder, '1-shape_match.png')
-            generate_image(shapes_test[i*4+1], colors_test[i*4+1],
-                           textures_test[i*4+1], img_file)
-            img_file = os.path.join(test_folder, '2-color_match.png')
-            generate_image(shapes_test[i*4+2], colors_test[i*4+2],
-                           textures_test[i*4+2], img_file)
-            img_file = os.path.join(test_folder, '3-texture_match.png')
-            generate_image(shapes_test[i*4+3], colors_test[i*4+3],
-                           textures_test[i*4+3], img_file)
 
-def get_test_set(data_folder, target_size):
-    contents = [elt for elt in os.listdir(data_folder)
-                if elt.startswith('test')]
-    contents = sorted(contents)
-    X = []
-    for dir in contents:
-        dir_path = os.path.join(data_folder, dir)
-        imgs = load_image_dataset(dir_path, target_size=target_size,
-                                  feature_info=False)
-        X.append(imgs)
-    X = np.concatenate(X)
+def make_trial(shapes, colors, textures):
+    # create a random trial
+    ix = np.arange(len(shapes))
 
-    return X
+    while True:
+        baseline = np.random.choice(ix)
+        shape = shapes[baseline]
+        color = colors[baseline]
+        texture = textures[baseline]
+        # only matches in shape
+        shape_matches = \
+        np.where((shapes == shape) &
+                 (colors != color) &
+                 (textures != texture))[0]
+        # only matches in color
+        color_matches = \
+        np.where((shapes != shape) &
+                 (colors == color) &
+                 (textures != texture))[0]
+        # only matches in texture
+        texture_matches = \
+        np.where((shapes != shape) &
+                 (colors != color) &
+                 (textures == texture))[0]
+
+        if len(shape_matches) > 0 and len(color_matches) > 0 and len(
+                texture_matches) > 0:
+            break  # make sure we have an option for each image...
+
+    shape_match = np.random.choice(shape_matches)
+    color_match = np.random.choice(color_matches)
+    texture_match = np.random.choice(texture_matches)
+
+    return [baseline, shape_match, color_match, texture_match]
+
+def build_test_trials(test_folder, nb_trials, target_size=(200, 200)):
+    # First, load the images
+    imgs = load_image_dataset(test_folder, target_size=target_size)
+    # Collect the list of shapes, colors and textures
+    feature_file = os.path.join(test_folder, 'data.csv')
+    df = pd.read_csv(feature_file, index_col=0)
+    shapes = df['shape'].as_matrix()
+    colors = df['color'].as_matrix()
+    textures = df['texture'].as_matrix()
+
+    # Sample the trials
+    ix = []
+    for i in range(nb_trials):
+        ix.extend(make_trial(shapes, colors, textures))
+    ix = np.asarray(ix)
+    return imgs[ix]
 
 def run_experiment(nb_categories, nb_exemplars, params):
     if params['gpu_options'] is not None:
@@ -87,17 +72,20 @@ def run_experiment(nb_categories, nb_exemplars, params):
             config=tf.ConfigProto(gpu_options=params['gpu_options'])
         )
         K.set_session(sess)
-    data_folder = os.path.realpath('../data/images_ca%0.4i_ex%0.4i' %
-                                   (nb_categories, nb_exemplars))
-    create_dataset(nb_categories, nb_exemplars, data_folder)
-    X, shapes = load_image_dataset(data_folder, target_size=params['img_size'])
+
+    # data_folder = os.path.realpath('../data/images_generated_old/images_ca0050_ex0014')
+    data_folder = os.path.realpath('../data/images_generated')
+    X, shapes = load_image_dataset(data_folder, nb_categories, nb_exemplars,
+                                       target_size=params['img_size'])
     ohe = OneHotEncoder(sparse=False)
     Y = ohe.fit_transform(shapes.reshape(-1, 1))
     # Now, we separate the train and test sets
     test_inds = [i*(nb_exemplars+1) for i in range(nb_categories)]
     # The train inds are the set difference of all inds and test inds
     train_inds = list(set(range(len(shapes))).difference(test_inds))
-    X_test = get_test_set(data_folder, target_size=params['img_size'])
+    test_folder = os.path.join(data_folder, 'test/')
+    X_test = build_test_trials(test_folder, nb_trials=1000,
+                               target_size=params['img_size'])
     # Build a neural network model and train it with the training set
     print('Training CNN model...')
     scores = []
