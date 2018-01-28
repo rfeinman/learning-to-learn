f7df8b562597f5e73ed45acdf8b0d5795380d130
Reuben Feinman <rfeinman16@gmail.com>
2018-01-27 21:37:02 -0500

    updates to mlp training code

--- learning2learn/old.py
+++ learning2learn/old.py
@@ -1,16 +1,110 @@
 from __future__ import division
 
 import os
+import math
+import itertools
+import warnings
 import numpy as np
 import pandas as pd
 from keras.preprocessing import image
 import matplotlib.pylab as plt
 import matplotlib.path as mplpath
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 from learning2learn.images import shift_image, generate_random_shape
 
 from learning2learn.mpl_textures import (Texture, generate_texture,
                                          get_base_image, add_texture)
+
+
+def generate_dictionary(column, nb_bits=None):
+    """
+    Observe the set of all values in a given feature column and create a
+    randomly-assigned dictionary with one bitmap vector for each value.
+    TODO: update this so that bitmap vectors are orthogonal
+    :param column: (Pandas Series) a feature column from your data set
+    :param nb_bits: (int) the number of bits to use in the bitmap vectors. If
+    None (default), the number is chosen based on the set of unique values in
+    the feature column.
+    :return: (dict) a dictionary containing one bitmap vector per feature value.
+    """
+    val_set = np.unique(column)
+    nb_vals = val_set.shape[0]
+    if nb_bits is None:
+        # compute the required number of bits based on the
+        # number of values. Let's add 2 bits for extra space.
+        nb_bits = int(round(math.log(nb_vals, 2))) + 2
+    else:
+        assert type(nb_bits) == int
+    # the candidate vectors
+    candidates = np.asarray([seq for seq in itertools.product([0,1],
+                                                              repeat=nb_bits)])
+    # the candidates that we will use are randomly selected
+    inds = np.random.choice(range(len(candidates)), nb_vals, replace=False)
+    # now select the vectors
+    vectors = candidates[inds]
+
+    return {val_set[i]: vectors[i] for i in range(nb_vals)}
+
+def convert_column(column, nb_bits=None):
+    dictionary = generate_dictionary(column, nb_bits)
+
+    return np.asarray([dictionary[elt] for elt in column])
+
+def preprocess_data(df, labels, one_hot=False, nb_bits=None):
+    # check on form of the dataframe
+    cols = df.columns.tolist()
+    assert len(cols) == 3
+    assert 'shape' in cols and 'color' in cols and 'texture' in cols
+    # warn if nb_bits is provided but will not be used
+    if one_hot and nb_bits is not None:
+        warnings.warn('nb_bits parameter is not used when one_hot=True.')
+    # encode the categorical variables
+    if one_hot:
+        # one-hot-encode the categorical variables in the data set
+        df = pd.get_dummies(df, columns=['shape', 'color', 'texture'])
+        X = df.values
+    else:
+        # select bitmap vectors for each categorical variable
+        shape = convert_column(df['shape'], nb_bits)
+        color = convert_column(df['color'], nb_bits)
+        texture = convert_column(df['texture'], nb_bits)
+        X = np.concatenate((shape, color, texture), axis=1)
+    # turn label words into number indices
+    le = LabelEncoder()
+    y = le.fit_transform(labels)
+    # one-hot-encode the labels so that they work with softmax output
+    ohe = OneHotEncoder(sparse=False)
+    Y = ohe.fit_transform(y.reshape(-1, 1))
+
+    return X, Y
+
+def synthesize_new_data(nb_categories):
+    """
+    Synthesize new object data for the second order generalization test.
+    Groupings of 4 samples are generated: first, a baseline example of the new
+    object category, and then 3 comparison samples. One of the comparison
+    samples maintains the same shape as the baseline, another the same
+    color, and another the same texture. For each, the other features are
+    different from the baseline.
+    :param nb_categories: (int) The number of categories in our original data
+                            set
+    :return: (Pandas DataFrame, Pandas Series) The data features and labels
+    """
+    # Create the first grouping
+    a = np.asarray([[nb_categories, nb_categories, nb_categories],
+                    [nb_categories, nb_categories+1, nb_categories+1],
+                    [nb_categories+1, nb_categories, nb_categories+2],
+                    [nb_categories+2, nb_categories+2, nb_categories]])
+    # Loop through, incrementing grouping by 3 each time and stacking them all
+    # on top of each other
+    dfs = []
+    labels = []
+    for i in range(nb_categories):
+        dfs.append(pd.DataFrame(a+3*i, columns=['shape', 'color', 'texture']))
+        labels.extend([nb_categories+3*i for _ in range(4)])
+
+    return pd.concat(dfs), pd.Series(labels)
 
 def get_train_test_inds(nb_categories, nb_exemplars, nb_samples, nb_test=1):
     test_inds = []
--- learning2learn/wrangle.py
+++ learning2learn/wrangle.py
@@ -7,75 +7,12 @@ import random
 import multiprocessing as mp
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 from learning2learn.util import train_test_split
 from learning2learn.images import (generate_image, generate_image_wrapper,
                                    generate_colors, generate_random_shape,
                                    compute_area)
 
-
-def generate_dictionary(column, nb_bits=None):
-    """
-    Observe the set of all values in a given feature column and create a
-    randomly-assigned dictionary with one bitmap vector for each value.
-    TODO: update this so that bitmap vectors are orthogonal
-    :param column: (Pandas Series) a feature column from your data set
-    :param nb_bits: (int) the number of bits to use in the bitmap vectors. If
-    None (default), the number is chosen based on the set of unique values in
-    the feature column.
-    :return: (dict) a dictionary containing one bitmap vector per feature value.
-    """
-    val_set = np.unique(column)
-    nb_vals = val_set.shape[0]
-    if nb_bits is None:
-        # compute the required number of bits based on the
-        # number of values. Let's add 2 bits for extra space.
-        nb_bits = int(round(math.log(nb_vals, 2))) + 2
-    else:
-        assert type(nb_bits) == int
-    # the candidate vectors
-    candidates = np.asarray([seq for seq in itertools.product([0,1],
-                                                              repeat=nb_bits)])
-    # the candidates that we will use are randomly selected
-    inds = np.random.choice(range(len(candidates)), nb_vals, replace=False)
-    # now select the vectors
-    vectors = candidates[inds]
-
-    return {val_set[i]: vectors[i] for i in range(nb_vals)}
-
-def convert_column(column, nb_bits=None):
-    dictionary = generate_dictionary(column, nb_bits)
-
-    return np.asarray([dictionary[elt] for elt in column])
-
-def preprocess_data(df, labels, one_hot=False, nb_bits=None):
-    # check on form of the dataframe
-    cols = df.columns.tolist()
-    assert len(cols) == 3
-    assert 'shape' in cols and 'color' in cols and 'texture' in cols
-    # warn if nb_bits is provided but will not be used
-    if one_hot and nb_bits is not None:
-        warnings.warn('nb_bits parameter is not used when one_hot=True.')
-    # encode the categorical variables
-    if one_hot:
-        # one-hot-encode the categorical variables in the data set
-        df = pd.get_dummies(df, columns=['shape', 'color', 'texture'])
-        X = df.values
-    else:
-        # select bitmap vectors for each categorical variable
-        shape = convert_column(df['shape'], nb_bits)
-        color = convert_column(df['color'], nb_bits)
-        texture = convert_column(df['texture'], nb_bits)
-        X = np.concatenate((shape, color, texture), axis=1)
-    # turn label words into number indices
-    le = LabelEncoder()
-    y = le.fit_transform(labels)
-    # one-hot-encode the labels so that they work with softmax output
-    ohe = OneHotEncoder(sparse=False)
-    Y = ohe.fit_transform(y.reshape(-1, 1))
-
-    return X, Y
 
 def synthesize_data(nb_categories, nb_exemplars):
     labels = []
@@ -92,32 +29,26 @@ def synthesize_data(nb_categories, nb_exemplars):
 
     return df, pd.Series(labels)
 
-def synthesize_new_data(nb_categories):
-    """
-    Synthesize new object data for the second order generalization test.
-    Groupings of 4 samples are generated: first, a baseline example of the new
-    object category, and then 3 comparison samples. One of the comparison
-    samples maintains the same shape as the baseline, another the same
-    color, and another the same texture. For each, the other features are
-    different from the baseline.
-    :param nb_categories: (int) The number of categories in our original data
-                            set
-    :return: (Pandas DataFrame, Pandas Series) The data features and labels
-    """
-    # Create the first grouping
-    a = np.asarray([[nb_categories, nb_categories, nb_categories],
-                    [nb_categories, nb_categories+1, nb_categories+1],
-                    [nb_categories+1, nb_categories, nb_categories+2],
-                    [nb_categories+2, nb_categories+2, nb_categories]])
-    # Loop through, incrementing grouping by 3 each time and stacking them all
-    # on top of each other
-    dfs = []
-    labels = []
-    for i in range(nb_categories):
-        dfs.append(pd.DataFrame(a+3*i, columns=['shape', 'color', 'texture']))
-        labels.extend([nb_categories+3*i for _ in range(4)])
+def generate_bit_dictionary(nb_categories, nb_bits=20):
+    min_bits = int(np.ceil(math.log(nb_categories, 2)))
+    assert nb_bits >= min_bits
+    # the candidate vectors
+    candidates = np.asarray(
+        [seq for seq in itertools.product([0, 1], repeat=nb_bits)]
+    )
+    # the candidates that we will use are randomly selected
+    ix = np.random.choice(
+        range(len(candidates)), nb_categories, replace=False
+    )
 
-    return pd.concat(dfs), pd.Series(labels)
+    return candidates[ix]
+
+def build_train_set_bits(df_train, shape_set, color_set, texture_set):
+    shape = np.asarray([shape_set[elt] for elt in df_train['shape']])
+    color = np.asarray([color_set[elt] for elt in df_train['color']])
+    texture = np.asarray([texture_set[elt] for elt in df_train['texture']])
+
+    return np.concatenate((shape, color, texture), axis=1)
 
 def build_train_set(df_train, shape_set, color_set, texture_set,
                     target_size=(200, 200), shift_scale=20, contrast_factor=1.):
@@ -175,6 +106,39 @@ def build_test_set_order1(
     labels = [sample[1] for sample in samples]
 
     return np.asarray(imgs), np.asarray(labels)
+
+def build_test_trials_o1_bits(df_train,
+                              shape_set_train, shape_set_test,
+                              color_set_train, color_set_test,
+                              texture_set_train, texture_set_test,
+                              nb_trials):
+    nb_bits = len(shape_set_train[0])
+    X_test = np.zeros((4 * nb_trials, 3 * nb_bits))
+    for i in range(nb_trials):
+        # First sample the baseline from the training set
+        ix = np.random.choice(range(len(df_train)), 1)[0]
+        s1, c1, t1 = df_train.iloc[ix]
+        shape1 = shape_set_train[s1]
+        color1 = color_set_train[c1]
+        texture1 = texture_set_train[t1]
+        # Now sample the 2 novel colors and textures that will be used
+        s2, s3 = np.random.choice(range(len(shape_set_test)), 2, replace=False)
+        shape2, shape3 = shape_set_test[s2], shape_set_test[s3]
+        c2, c3 = np.random.choice(range(len(color_set_test)), 2, replace=False)
+        color2, color3 = color_set_test[c2], color_set_test[c3]
+        t2, t3 = np.random.choice(range(len(texture_set_test)), 2,
+                                  replace=False)
+        texture2, texture3 = texture_set_test[t2], texture_set_test[t3]
+        # Now build the samples
+        baseline = np.concatenate((shape1, color1, texture1))
+        shape_match = np.concatenate((shape1, color2, texture2))
+        color_match = np.concatenate((shape2, color1, texture3))
+        texture_match = np.concatenate((shape3, color3, texture1))
+        # Collect trial and return
+        X_test[4 * i:4 * (i + 1)] = np.asarray(
+            [baseline, shape_match, color_match, texture_match])
+
+    return X_test
 
 def make_trial_order1(df_train,
                       shape_set_train, shape_set_test,
@@ -238,6 +202,29 @@ def build_test_trials_order1(df_train,
 
     return np.concatenate(trials)
 
+def build_test_trials_o2_bits(shape_set, color_set, texture_set, nb_trials):
+    nb_bits = len(shape_set[0])
+    X_test = np.zeros((4 * nb_trials, 3 * nb_bits))
+    for i in range(nb_trials):
+        # randomly select 3 of each feature
+        s1, s2, s3 = np.random.choice(range(len(shape_set)), 3, replace=False)
+        c1, c2, c3 = np.random.choice(range(len(color_set)), 3, replace=False)
+        t1, t2, t3 = np.random.choice(range(len(texture_set)), 3, replace=False)
+        shape1, shape2, shape3 = shape_set[s1], shape_set[s2], shape_set[s3]
+        color1, color2, color3 = color_set[c1], color_set[c2], color_set[c3]
+        texture1, texture2, texture3 = texture_set[c1], texture_set[c2], \
+                                       texture_set[c3]
+        # Now build the samples
+        baseline = np.concatenate((shape1, color1, texture1))
+        shape_match = np.concatenate((shape1, color2, texture2))
+        color_match = np.concatenate((shape2, color1, texture3))
+        texture_match = np.concatenate((shape3, color3, texture1))
+        # Collect trial and return
+        X_test[4 * i:4 * (i + 1)] = np.asarray(
+            [baseline, shape_match, color_match, texture_match])
+
+    return X_test
+
 def make_trial_order2(shape_set, color_set, texture_set, target_size=(200, 200),
                       shift_scale=20, contrast_factor=1.):
     # randomly select 3 of each feature
@@ -279,26 +266,32 @@ def build_test_trials_order2(shape_set, color_set, texture_set, nb_trials,
 
     return np.concatenate(trials)
 
-def get_train_test_parameters(img_size=200):
+def get_train_test_parameters(images=True, img_size=200, nb_bits=20):
     # we have 58 textures, so that is our limiting factor. We will use 50 for
     # training and hold out 8 for test.
     nb_train = 50
     nb_test = 8
-    # get the 58 shapes
-    shape_set = [generate_random_shape(0, img_size, 0, img_size,
-                                       int(img_size/5.))
-                 for _ in range(nb_train+nb_test)]
-    shape_set = sorted(shape_set, key=lambda x: compute_area(x, img_size))
-    # get the 58 colors
-    color_set = generate_colors()
-    ix = np.sort(np.random.choice(range(len(color_set)), nb_train+nb_test,
+    if images:
+        # get the set of shapes
+        shape_set = [generate_random_shape(0, img_size, 0, img_size,
+                                           int(img_size/5.))
+                     for _ in range(nb_train+nb_test)]
+        shape_set = sorted(shape_set, key=lambda x: compute_area(x, img_size))
+        # get the set of colors
+        color_set = generate_colors()
+        ix = np.sort(np.random.choice(range(len(color_set)), nb_train+nb_test,
                                   replace=False))
-    color_set = color_set[ix]
-    # get the 58 textures
-    texture_set = sorted(
-        [file for file in os.listdir('../data/textures') if
-         file.endswith('tiff')]
-    )
+        color_set = color_set[ix]
+        # get the set of textures
+        texture_set = sorted(
+            [file for file in os.listdir('../data/textures') if
+             file.endswith('tiff')]
+        )
+    else:
+        # get the set of shapes, colors and textures
+        shape_set = generate_bit_dictionary(nb_train+nb_test, nb_bits)
+        color_set = generate_bit_dictionary(nb_train+nb_test, nb_bits)
+        texture_set = generate_bit_dictionary(nb_train+nb_test, nb_bits)
     # perform the train/test splits
     shape_set_train, shape_set_test = train_test_split(shape_set,
                                                        test_size=nb_test)
@@ -310,3 +303,35 @@ def get_train_test_parameters(img_size=200):
     return (shape_set_train, shape_set_test), \
            (color_set_train, color_set_test), \
            (texture_set_train, texture_set_test)
+
+# def get_train_test_parameters(img_size=200):
+#     # we have 58 textures, so that is our limiting factor. We will use 50 for
+#     # training and hold out 8 for test.
+#     nb_train = 50
+#     nb_test = 8
+#     # get the 58 shapes
+#     shape_set = [generate_random_shape(0, img_size, 0, img_size,
+#                                        int(img_size/5.))
+#                  for _ in range(nb_train+nb_test)]
+#     shape_set = sorted(shape_set, key=lambda x: compute_area(x, img_size))
+#     # get the 58 colors
+#     color_set = generate_colors()
+#     ix = np.sort(np.random.choice(range(len(color_set)), nb_train+nb_test,
+#                                   replace=False))
+#     color_set = color_set[ix]
+#     # get the 58 textures
+#     texture_set = sorted(
+#         [file for file in os.listdir('../data/textures') if
+#          file.endswith('tiff')]
+#     )
+#     # perform the train/test splits
+#     shape_set_train, shape_set_test = train_test_split(shape_set,
+#                                                        test_size=nb_test)
+#     color_set_train, color_set_test = train_test_split(color_set,
+#                                                        test_size=nb_test)
+#     texture_set_train, texture_set_test = train_test_split(texture_set,
+#                                                            test_size=nb_test)
+#
+#     return (shape_set_train, shape_set_test), \
+#            (color_set_train, color_set_test), \
+#            (texture_set_train, texture_set_test)
