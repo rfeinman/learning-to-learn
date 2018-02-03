b746e23725164777a3dd55be0efc8be3fe59b962
Reuben Feinman <rfeinman16@gmail.com>
2018-02-03 13:02:07 -0500

    cleaning up scripts

--- scripts/vocabulary_acceleration_multi.py
+++ scripts/vocabulary_acceleration_multi.py
@@ -10,16 +10,29 @@ import tensorflow as tf
 import keras.backend as K
 from sklearn.preprocessing import OneHotEncoder
 
-from learning2learn.util import evaluate_generalization, train_test_split
-from learning2learn.wrangle import (synthesize_data, get_train_test_parameters,
+from learning2learn.util import (evaluate_generalization, train_test_split, get_hidden_representations)
+from learning2learn.wrangle import (get_train_test_parameters,
                                     build_test_trials_order2, build_train_set)
-from learning2learn.models import simple_cnn
+from learning2learn.models import simple_cnn_multiout
 
 
-def get_correct_classes(model, X_train, Y_train, nb_categories, threshold=0.8):
+def synthesize_data_multi(nb_shapes, nb_colors, nb_textures, nb_exemplars):
+    shapes = []
+    for i in range(nb_shapes):
+        shapes.extend([i for _ in range(nb_exemplars)])
+    colors = np.random.choice(range(nb_colors), size=nb_shapes*nb_exemplars)
+    textures = np.random.choice(range(nb_textures), size=nb_shapes*nb_exemplars)
+    df = pd.DataFrame(columns=['shape', 'color', 'texture'])
+    df['shape'] = shapes
+    df['color'] = colors
+    df['texture'] = textures
+
+    return df
+
+def get_correct_classes(Y_train, Y_pred, nb_categories, threshold=0.8):
     # get predictions and ground truth
     y_true = Y_train.argmax(axis=1)
-    y_pred = model.predict_classes(X_train, batch_size=128)
+    y_pred = Y_pred.argmax(axis=1)
     # collect accuracies for each class
     classes = np.arange(nb_categories)
     accuracies = np.zeros_like(classes, dtype=np.float32)
@@ -30,61 +43,67 @@ def get_correct_classes(model, X_train, Y_train, nb_categories, threshold=0.8):
     # collect vocabulary and return
     return np.where(accuracies >= threshold)[0]
 
-def save_scores(epoch, trainLoss, trainAcc, o2_correct, vocabSize50,
-                vocabSize80, logfile):
+def save_scores(epoch, o2_correct, vocabSize50_s, vocabSize80_s,
+                vocabSize50_t, vocabSize80_t, logfile):
     df = pd.DataFrame()
     df['epoch'] = epoch
-    df['trainLoss'] = trainLoss
-    df['trainAcc'] = trainAcc
     df['o2_correct'] = o2_correct
-    df['vocabSize50'] = vocabSize50
-    df['vocabSize80'] = vocabSize80
+    df['vocabSize50_s'] = vocabSize50_s
+    df['vocabSize80_s'] = vocabSize80_s
+    df['vocabSize50_t'] = vocabSize50_t
+    df['vocabSize80_t'] = vocabSize80_t
     df.to_csv(logfile, index=False)
 
-def evaluate_model(model, X_train, Y_train, X_test, v50, v80):
-    loss, acc = model.evaluate(X_train, Y_train, batch_size=256, verbose=0)
-    acc2 = evaluate_generalization(
-        model, X_test, layer_num=-4,
-        batch_size=256
+def evaluate_model(model, X_train, Y_train_s, Y_train_t,
+                   X_test, v50_s, v80_s, v50_t, v80_t):
+    acc_o2 = evaluate_generalization(
+        model, X_test, layer_num=-5,
+        batch_size=128
     )
-    correct50 = get_correct_classes(
-        model, X_train, Y_train, nb_categories=Y_train.shape[-1], threshold=0.5
+    Y_pred_s = get_hidden_representations(model, X_train, layer_num=-3,
+                                          batch_size=128)
+    Y_pred_t = get_hidden_representations(model, X_train, layer_num=-1,
+                                          batch_size=128)
+    correct50_s = get_correct_classes(
+        Y_train_s, Y_pred_s, nb_categories=Y_train_s.shape[-1], threshold=0.5
     )
-    correct80 = get_correct_classes(
-        model, X_train, Y_train, nb_categories=Y_train.shape[-1], threshold=0.8
+    correct80_s = get_correct_classes(
+        Y_train_s, Y_pred_s, nb_categories=Y_train_s.shape[-1], threshold=0.8
     )
-    v50 = v50.union(correct50)
-    v80 = v80.union(correct80)
+    correct50_t = get_correct_classes(
+        Y_train_t, Y_pred_t, nb_categories=Y_train_t.shape[-1], threshold=0.5
+    )
+    correct80_t = get_correct_classes(
+        Y_train_t, Y_pred_t, nb_categories=Y_train_t.shape[-1], threshold=0.8
+    )
+    v50_s = v50_s.union(correct50_s)
+    v80_s = v80_s.union(correct80_s)
+    v50_t = v50_t.union(correct50_t)
+    v80_t = v80_t.union(correct80_t)
 
-    return loss, acc, acc2, v50, v80
+    return acc_o2, v50_s, v80_s, v50_t, v80_t
 
-def get_data(nb_categories, nb_exemplars):
+def get_data(nb_shapes, nb_colors, nb_textures, nb_exemplars):
     print('Building image datasets...')
-    # Get the training data
-    df_train, labels = synthesize_data(
-        nb_categories=nb_categories,
-        nb_exemplars=nb_exemplars
-    )
-    ohe = OneHotEncoder(sparse=False)
-    Y_train = ohe.fit_transform(labels.values.reshape(-1, 1))
     # Get the shape, color and texture features
     (shape_set_train, shape_set_test), \
     (color_set_train, color_set_test), \
     (texture_set_train, texture_set_test) = \
         get_train_test_parameters(images=True, img_size=200)
-    if nb_categories < 50:
-        shape_set_train, _ = train_test_split(
-            shape_set_train,
-            50 - nb_categories
-        )
-        color_set_train, _ = train_test_split(
-            color_set_train,
-            50 - nb_categories
-        )
-        texture_set_train, _ = train_test_split(
-            texture_set_train,
-            50 - nb_categories
-        )
+    if nb_shapes < 50:
+        shape_set_train, _ = train_test_split(shape_set_train, 50-nb_shapes)
+    if nb_colors < 50:
+        color_set_train, _ = train_test_split(color_set_train, 50-nb_colors)
+    if nb_textures < 50:
+        texture_set_train, _ = train_test_split(texture_set_train, 50-nb_textures)
+    # Get the training data
+    df_train = synthesize_data_multi(
+        nb_shapes, nb_colors, nb_textures, nb_exemplars
+    )
+    ohe = OneHotEncoder(sparse=False)
+    Y_train_s = ohe.fit_transform(df_train['shape'].values.reshape(-1, 1))
+    Y_train_c = ohe.fit_transform(df_train['color'].values.reshape(-1, 1))
+    Y_train_t = ohe.fit_transform(df_train['texture'].values.reshape(-1, 1))
     # Build the training set of images
     X_train = build_train_set(
         df_train, shape_set_train,
@@ -98,10 +117,16 @@ def get_data(nb_categories, nb_exemplars):
         shift_scale=20
     )
 
-    return X_train, Y_train, X_test
+    return X_train, Y_train_s, Y_train_c, Y_train_t, X_test
 
 def main():
-    assert args.nb_categories <= 50
+    nb_shapes = int(np.round(args.shape_fraction*args.nb_categories))
+    nb_colors = int(np.round(args.color_fraction*args.nb_categories))
+    nb_textures = args.nb_categories - nb_shapes - nb_colors
+    texture_fraction = 1. - args.shape_fraction - args.color_fraction
+    print('nb_shapes: %i' % nb_shapes)
+    print('nb_colors: %i' % nb_colors)
+    print('nb_textures: %i' % nb_textures)
     if args.gpu_num is not None:
         gpu_options = tf.GPUOptions(allow_growth=True,
                                     visible_device_list=args.gpu_num)
@@ -118,20 +143,18 @@ def main():
     os.mkdir(args.save_path)
     if not args.varying_data:
         print('Not varying the data across runs.')
-        X_train, Y_train, X_test = get_data(args.nb_categories,
-                                            args.nb_exemplars)
+        X_train, Y_train_s, Y_train_c, Y_train_t, X_test = \
+            get_data(nb_shapes, nb_colors, nb_textures, args.nb_exemplars)
     for i in range(args.nb_runs):
+        print('Run #%i' % i)
         if args.varying_data:
             print('Selecting data for run %i' % i)
-            X_train, Y_train, X_test = get_data(args.nb_categories,
-                                                args.nb_exemplars)
-        # enforce a maximum batch size to avoid doing full-batch GD
-        batch_size = min(
-            args.batch_size,
-            int(np.floor(args.nb_categories*args.nb_exemplars / 5))
-        )
-        v50 = set([])  # keep track of vocab w/ threshold = 50
-        v80 = set([])  # keep track of vocab w/ threshold = 80
+            X_train, Y_train_s, Y_train_c, Y_train_t, X_test = \
+                get_data(nb_shapes, nb_colors, nb_textures, args.nb_exemplars)
+        v50_s = set([])  # keep track of shape vocab w/ threshold = 50
+        v80_s = set([])  # keep track of shape vocab w/ threshold = 80
+        v50_t = set([])  # keep track of texture vocab w/ threshold = 50
+        v80_t = set([])  # keep track of texture vocab w/ threshold = 80
         save_file = os.path.join(
             args.save_path,
             'run_%0.2i.csv' % i
@@ -139,50 +162,58 @@ def main():
         # initialize the model and compute initial metrics
         print('Initializing the model and computing initial metrics...')
         epoch = []
-        trainLoss = []
-        trainAcc = []
         o2_correct = []
-        vocabSize50 = []
-        vocabSize80 = []
-        model = simple_cnn(
+        vocabSize50_s = []
+        vocabSize80_s = []
+        vocabSize50_t = []
+        vocabSize80_t = []
+        model = simple_cnn_multiout(
             input_shape=X_train.shape[1:],
-            nb_classes=Y_train.shape[-1]
+            nb_shapes=Y_train_s.shape[-1],
+            nb_colors=Y_train_c.shape[-1],
+            nb_textures=Y_train_t.shape[-1],
+            loss_weights=[args.shape_fraction, args.color_fraction,
+                          texture_fraction]
         )
-        loss, acc, acc2, v50, v80 = evaluate_model(
-            model, X_train, Y_train, X_test, v50, v80
+        acc_o2, v50_s, v80_s, v50_t, v80_t = evaluate_model(
+            model, X_train, Y_train_s, Y_train_t,
+            X_test, v50_s, v80_s, v50_t, v80_t
         )
-        nb_correct = int(np.round(acc2*args.nb_test))
-        print('initial shape bias: %0.3f' % acc2)
+        nb_correct = int(np.round(acc_o2*args.nb_test))
+        print('initial # shape selections: %0.3f' % nb_correct)
         epoch.append(0)
-        trainLoss.append(loss)
-        trainAcc.append(acc)
         o2_correct.append(nb_correct)
-        vocabSize50.append(len(v50))
-        vocabSize80.append(len(v80))
+        vocabSize50_s.append(len(v50_s))
+        vocabSize80_s.append(len(v80_s))
+        vocabSize50_t.append(len(v50_t))
+        vocabSize80_t.append(len(v80_t))
         save_scores(
-            epoch, trainLoss, trainAcc, o2_correct,
-            vocabSize50, vocabSize80, save_file
+            epoch, o2_correct, vocabSize50_s, vocabSize80_s,
+            vocabSize50_t, vocabSize80_t, save_file
         )
         for j in range(args.nb_epochs):
             print('Epoch #%i' % (j + 1))
             model.fit(
-                X_train, Y_train, epochs=1,
-                shuffle=True,
-                verbose=2, batch_size=batch_size
+                [X_train],
+                [Y_train_s, Y_train_c, Y_train_t],
+                epochs=1,
+                batch_size=args.batch_size,
+                verbose=2
             )
-            loss, acc, acc2, v50, v80 = evaluate_model(
-                model, X_train, Y_train, X_test, v50, v80
+            acc_o2, v50_s, v80_s, v50_t, v80_t = evaluate_model(
+                model, X_train, Y_train_s, Y_train_t,
+                X_test, v50_s, v80_s, v50_t, v80_t
             )
-            nb_correct = int(np.round(acc2*args.nb_test))
-            epoch.append(j + 1)
-            trainLoss.append(loss)
-            trainAcc.append(acc)
+            nb_correct = int(np.round(acc_o2*args.nb_test))
+            epoch.append(j+1)
             o2_correct.append(nb_correct)
-            vocabSize50.append(len(v50))
-            vocabSize80.append(len(v80))
+            vocabSize50_s.append(len(v50_s))
+            vocabSize80_s.append(len(v80_s))
+            vocabSize50_t.append(len(v50_t))
+            vocabSize80_t.append(len(v80_t))
             save_scores(
-                epoch, trainLoss, trainAcc, o2_correct,
-                vocabSize50, vocabSize80, save_file
+                epoch, o2_correct, vocabSize50_s, vocabSize80_s,
+                vocabSize50_t, vocabSize80_t, save_file
             )
     if args.gpu_num is not None:
         K.clear_session()
@@ -193,12 +224,12 @@ if __name__ == '__main__':
     parser.add_argument('-ep', '--nb_epochs',
                         help='The number of epochs to train for.',
                         required=False, type=int)
-    # parser.add_argument('-sf', '--shape_fraction',
-    #                     help='The fraction to separate by shape.',
-    #                     required=False, type=float)
-    # parser.add_argument('-cf', '--color_fraction',
-    #                     help='The fraction to separate by color.',
-    #                     required=False, type=float)
+    parser.add_argument('-sf', '--shape_fraction',
+                        help='The fraction to separate by shape.',
+                        required=False, type=float)
+    parser.add_argument('-cf', '--color_fraction',
+                        help='The fraction to separate by color.',
+                        required=False, type=float)
     parser.add_argument('-ca', '--nb_categories',
                         help='The number of categories.',
                         required=False, type=int)
@@ -224,19 +255,16 @@ if __name__ == '__main__':
                         help='Whether to vary the data accross each model run.',
                         required=False, action='store_true')
     parser.set_defaults(nb_epochs=100)
-    # parser.set_defaults(shape_fraction=1.0)
-    # parser.set_defaults(color_fraction=0.0)
+    parser.set_defaults(shape_fraction=1.0)
+    parser.set_defaults(color_fraction=0.0)
     parser.set_defaults(nb_categories=15)
     parser.set_defaults(nb_exemplars=15)
     parser.set_defaults(save_path='../results/vocab_log')
     parser.set_defaults(gpu_num=None)
-    parser.set_defaults(batch_size=32)
+    parser.set_defaults(batch_size=10)
     parser.set_defaults(nb_runs=5)
     parser.set_defaults(nb_test=100)
     parser.set_defaults(varying_data=False)
     args = parser.parse_args()
-    print('nb_categories: %i, nb_exemplars: %i, nb_runs: %i, nb_test: %i, '
-          'varying_data: %r' %
-          (args.nb_categories, args.nb_exemplars, args.nb_runs, args.nb_test,
-           args.varying_data))
+    print(args)
     main()
